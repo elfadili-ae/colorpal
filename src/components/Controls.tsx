be1ae7b6280f7bb672154923152e596f7067c076
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-06-27 22:54:31 +0000

    feat: display colors and controls

---
+++ src/components/Controls.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+type Props = {
+  prev:() => void,
+  next: () => void
+};
+
+const Controls: React.FC<Props> = ({prev, next}) => {
+  return (
+    <div className='w-full h-20 flex gap-1 bg-white p-1'>
+      <div onClick={() => prev()} className='cursor-pointer w-full h-full bg-black flex-1 flex justify-center items-center'>
+        <svg className='w-10 h-10' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path className='stroke-white' d="M4 12H20M4 12L8 8M4 12L8 16" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
+
+      <div onClick={() => next()} className='cursor-pointer w-full h-full bg-black flex-1 flex justify-center items-center'>
+        <svg className='w-10 h-10' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path className='stroke-white' d="M6 12H18M18 12L13 7M18 12L13 17" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
+    </div>
+  )
+}
+
+export default Controls
---
+++ src/components/Palette.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import React, { useEffect, useRef, useState } from 'react'
+import palettColors from '../data/palettes.json';
+import Controls from './Controls';
+
+const Palette = () => {
+    const first = useRef<HTMLDivElement>(null)
+    const second = useRef<HTMLDivElement>(null)
+    const third = useRef<HTMLDivElement>(null)
+    const fourth = useRef<HTMLDivElement>(null)
+    const fifth = useRef<HTMLDivElement>(null)
+
+    const [used, setUsed] = useState({});
+    const [currentColor, setCurrentColor] = useState(0);
+
+    const palettes = [first, second, third, fourth, fifth];
+
+    const setColors = (colors: string[]) : void => {
+        palettes.forEach((palette, i) => {
+            if (palette.current && colors[i]) {
+                palette.current.style.backgroundColor = colors[i];
+                palette.current.children[0].textContent = colors[i];
+            }
+        })
+    }
+
+    const copyColor = (e: React.MouseEvent<HTMLDivElement>) => {
+        const col = (e.target as HTMLDivElement).children[0].textContent as string;
+        navigator.clipboard.writeText(col);
+        (e.target as HTMLDivElement).classList.add('clicked');
+        (e.target as HTMLDivElement).style.opacity = '0.5';
+        setTimeout(() => {
+            (e.target as HTMLDivElement).classList.remove('clicked');
+        (e.target as HTMLDivElement).style.opacity = '1.0';
+        }, 200);
+    }
+
+    const nextColor = (): void => {
+        if (currentColor+1 === palettColors.length) {
+            setCurrentColor(0);
+        } else {
+            setCurrentColor(currentColor + 1);
+        }
+    }
+
+    const prevColor = (): void => {
+        if (currentColor === 0) {
+            setCurrentColor(palettColors.length - 1);
+        } else {
+            setCurrentColor(currentColor - 1);
+        }
+    }
+
+    useEffect(() => {
+        setColors(palettColors[currentColor]);
+    }, [currentColor]);
+    
+  return (
+    <div className='w-full h-screen flex flex-col'>
+      <Controls
+      next={nextColor}
+      prev={prevColor}
+      />
+        <div className='w-full h-full grid max-sm:grid-rows-5 sm:grid-cols-5'>
+            <div onClick={copyColor} ref={first} className='cursor-pointer w-full h-full flex justify-center items-end p-20'>
+                <p className='font-bold'>color</p>
+            </div>
+            <div onClick={copyColor} ref={second} className='cursor-pointer w-full h-full flex justify-center items-end p-20'>
+                <p className='font-bold'>color</p>
+            </div>
+            <div onClick={copyColor} ref={third} className='cursor-pointer w-full h-full flex justify-center items-end p-20'>
+                <p className='font-bold'>color</p>
+            </div>
+            <div onClick={copyColor} ref={fourth} className='cursor-pointer w-full h-full flex justify-center items-end p-20'>
+                <p className='font-bold'>color</p>
+            </div>
+            <div onClick={copyColor} ref={fifth} className='cursor-pointer w-full h-full flex justify-center items-end p-20'>
+                <p className='font-bold'>color</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Palette
